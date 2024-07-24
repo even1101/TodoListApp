8ebe11887cd4576228ac0508dfd3c44afdeee40b
even.chang <even1101u@gmail.com>
2024-07-24 18:26:04 +0800

    Remove folders

--- todolistapp/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/Objects-normal/arm64/Pods-Runner-dummy.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/even/Desktop/code/flutter/TodoListApp/todolistapp/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/module.modulemap \
-  /Users/even/Desktop/code/flutter/TodoListApp/todolistapp/macos/Pods/Target\ Support\ Files/Pods-Runner/Pods-Runner-dummy.m
--- todolistapp/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/Objects-normal/arm64/Pods_Runner_vers.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/even/Desktop/code/flutter/TodoListApp/todolistapp/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/module.modulemap \
-  /Users/even/Desktop/code/flutter/TodoListApp/todolistapp/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/DerivedSources/Pods_Runner_vers.c
